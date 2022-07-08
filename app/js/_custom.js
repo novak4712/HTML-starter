80286aecb1243a0a026630f4514f74557e3315db
sergei.sorokin <3wkNWhdM87Dw>
2022-07-08 15:17:44 +0300

    initial

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,3 +1,26 @@
-$(function() {
+document.addEventListener("DOMContentLoaded", function () {
+    /* const */
+    const body = $('body');
+
+    /*nav*/
+    body.on('click', '.nav-burger', function () {
+        $(this).toggleClass('active');
+        $('nav').toggleClass('active');
+    });
+
+
+    /* button-effects*/
+    $(".ripple").on("click", function (event) {
+        $(this).append("<span class='ripple-effect'>");
+        $(this).find(".ripple-effect").css({
+            left: event.pageX - $(this).position().left,
+            top: event.pageY - $(this).position().top
+        }).animate({
+            opacity: 0,
+        }, 1500, function () {
+            $(this).remove();
+        });
+    });
+
 
 });
